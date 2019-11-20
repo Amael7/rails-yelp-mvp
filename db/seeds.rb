dd98a65316bd84126f5e92290371d5225a2f27cc
Stéphane <stephane.26@hotmail.fr>
2019-11-20 17:00:55 +0100

    ex is over

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Restaurant1 = Restaurant.new(name: 'rolland', address: 'avenue du lol', phone_number: '01 02 03 04 05', category: 'japanese' )
-Restaurant2 = Restaurant.new(name: 'barba', address: 'avenue du rele', phone_number: '01 22 33 04 05', category: 'chinese' )
-Restaurant3 = Restaurant.new(name: 'popa', address: 'avenue du dbb', phone_number: '01 32 43 04 05', category: 'italian' )
-Restaurant4 = Restaurant.new(name: 'roly', address: 'avenue du sne', phone_number: '01 62 53 04 05', category: 'belgian' )
-Restaurant5 = Restaurant.new(name: 'magoly', address: 'avenue du pikachu', phone_number: '01 12 13 04 05', category: 'french' )
+restaurant1 = Restaurant.create(name: 'rolland', address: 'avenue du bulbizare', phone_number: '01 02 03 04 05', category: 'japanese' )
+restaurant2 = Restaurant.create(name: 'barba', address: 'avenue du salameche', phone_number: '01 22 33 04 05', category: 'chinese' )
+restaurant3 = Restaurant.create(name: 'popa', address: 'avenue du carapuce', phone_number: '01 32 43 04 05', category: 'italian' )
+restaurant4 = Restaurant.create(name: 'roly', address: 'avenue du evoli', phone_number: '01 62 53 04 05', category: 'belgian' )
+restaurant5 = Restaurant.create(name: 'magoly', address: 'avenue du pikachu', phone_number: '01 12 13 04 05', category: 'french' )
+
+p Restaurant.all
