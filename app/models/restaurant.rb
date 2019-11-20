2c846c5ad2d082ddcd3610fbbd48e3e6bf1766df
Stéphane <stephane.26@hotmail.fr>
2019-11-20 13:04:56 +0100

    test rollback phone number

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
-  has_many :review
+  has_many :review, dependent: :destroy
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  # validates :phone_number, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }, presence: true
 end
