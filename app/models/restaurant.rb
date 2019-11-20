ee587566e130e51c43161edc4647c27860de9758
Stéphane <stephane.26@hotmail.fr>
2019-11-20 15:47:59 +0100

    green rake

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,5 +4,5 @@ class Restaurant < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
   # validates :phone_number, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }, presence: true
 end
